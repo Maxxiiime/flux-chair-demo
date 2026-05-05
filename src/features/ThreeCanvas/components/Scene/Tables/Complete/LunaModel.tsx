b6af6909bc0c2b921988a4df7cb43aee32f055a3
Maxxiiime <max2002guillon@gmail.com>
2026-05-05 17:01:34 +0200

    feat: use Custom mesh to allow color change

--- src/features/ThreeCanvas/components/Scene/Tables/Complete/LunaModel.tsx
+++ src/features/ThreeCanvas/components/Scene/Tables/Complete/LunaModel.tsx
@@ -1,35 +1,38 @@
 import { useGLTF } from '@react-three/drei'
 import { useTableMaterial } from '@/hooks/useMaterial';
-import CustomMesh from '../../CustomMesh';
+import CustomMeshBase from '../../CustomMesh';
+import CustomMeshDebug from '../../CustomMeshDebug';
 
 export function LunaModel() {
   const { nodes: nodesTop } = useGLTF('/model/Tables/Luna-model-1-180_micro-cement_TOP.glb') as any;
   const { nodes: nodesBottom } = useGLTF('/model/Tables/Luna-model-1-180_micro-cement_BOTTOM.glb') as any;
   const tableMaterial = useTableMaterial();
 
+  const CustomMesh = import.meta.env.DEV ? CustomMeshDebug : CustomMeshBase;
+
   return (
-    <group dispose={null}>
+    <group dispose={null} position={[0, -0.1, -0.2]}>
       {/* TOP */}
       <CustomMesh
         castShadow
         receiveShadow
         geometry={nodesTop['Luna-model-1-180_micro-cement_TOP'].geometry}
         materialData={tableMaterial}
-        mode={1}
+        mode={0}
       />
       <CustomMesh
         castShadow
         receiveShadow
         geometry={nodesTop['Luna-model-1-180_micro-cement_TOP001'].geometry}
         materialData={tableMaterial}
-        mode={1}
+        mode={0}
       />
       <CustomMesh
         castShadow
         receiveShadow
         geometry={nodesTop['Luna-model-1-180_micro-cement_TOP002'].geometry}
         materialData={tableMaterial}
-        mode={1}
+        mode={0}
       />
 
       {/* BOTTOM */}
@@ -38,14 +41,14 @@ export function LunaModel() {
         receiveShadow
         geometry={nodesBottom['Luna-model-1-180_micro-cement_BOTTOM'].geometry}
         materialData={tableMaterial}
-        mode={1}
+        mode={0}
       />
       <CustomMesh
         castShadow
         receiveShadow
         geometry={nodesBottom['Luna-model-1-180_micro-cement_LEG'].geometry}
         materialData={tableMaterial}
-        mode={1}
+        mode={0}
       />
     </group>
   )
